f313aaf8a3bdfc3bccccda081edf251da60a6047
Hailey June <hailey.june.b@gmail.com>
2018-11-28 16:00:30 -0800

    Added second player and finished it off

--- Player.cs
+++ Player.cs
@@ -16,6 +16,13 @@ namespace CardGame
             health = 100;
         }
 
+        public Player(string myName, int bossHealth)
+        {
+            name = myName;
+            hand = new List<Card>();
+            health = bossHealth;
+        }
+
         public Card Draw(Deck thisDeck)
         {
             Card myCard = thisDeck.Deal();
@@ -35,23 +42,23 @@ namespace CardGame
 
         public void DisplayHand()
         {
-            System.Console.WriteLine("Your cards are: ");
+            System.Console.WriteLine("***   {0}'s cards are: ", name);
             for (int i = 0; i < hand.Count; i++)
             {
-            System.Console.WriteLine("-----------------------------------");
-            System.Console.WriteLine("|The {0} of {1}", hand[i].stringVal, hand[i].suit);
-            System.Console.WriteLine("|" + hand[i].description);
-            System.Console.WriteLine("-----------------------------------");
+            System.Console.WriteLine("***   -----------------------------------");
+            System.Console.WriteLine("***   |The {0} of {1}", hand[i].stringVal, hand[i].suit);
+            System.Console.WriteLine("***   |" + hand[i].description);
+            System.Console.WriteLine("***   -----------------------------------");
             }
         }
 
-        public void Attack(Card play, Boss target)
+        public void Attack(Card play, Player target)
         {
             int val = play.val;
             if (val > 1 && val < 11)
             {
                 target.health = target.health - val;
-                System.Console.WriteLine("{0} was attacked for {1} damage.", target.name, val);
+                System.Console.WriteLine("{0} was attacked by {2} for {1} damage.", target.name, val, name);
                 Discard(0);
 
             }
@@ -60,12 +67,12 @@ namespace CardGame
                 if (health + 10 >= 100)
                 {
                     health = 100;
-                    System.Console.WriteLine("{0} was healed to full health.", name);
+                    System.Console.WriteLine("{0} was healed to full health by their Ace.", name);
                 }
                 else
                 {
                     health += 10;
-                    System.Console.WriteLine("{0} was healed for 10HP.", name);
+                    System.Console.WriteLine("{0} was healed by their Ace for 10HP.", name);
                 }
                 Discard(0);
             }
@@ -74,25 +81,25 @@ namespace CardGame
                 Random rand = new Random();
                 int damage = rand.Next(1,10);
                 target.health = target.health - damage;
-                System.Console.WriteLine("{0} was attacked by a wildcard for {1} damage.", target.name, damage);
+                System.Console.WriteLine("{0} was attacked by {2}'s Jack for {1} damage.", target.name, damage, name);
                 Discard(0);
             }
             else if (val == 12)
             {
                 target.health -= 5;
-                System.Console.WriteLine("{0} Played a Queen, dealing 5 damage to {1} and allowing them to play another card!", name, target.name);
+                System.Console.WriteLine("{0} Played a Queen, dealing 5 damage to {1} and allowing {0} to play their other card!", name, target.name);
                 Attack(QueenCheat(0), target);
             }
             else if (val == 13)
             {
                 if (target.health < (target.health/4))
                 {
-                    System.Console.WriteLine("{0} was dealt a killing blow.");
+                    System.Console.WriteLine("{0} was dealt a killing blow by {1}'s King.", target.name, name);
                     target.health = 0;
                 }
                 else
                 {
-                    System.Console.WriteLine("The king attacked {0} but it had no effect.", target.name);
+                    System.Console.WriteLine("The {0}'s King attacked {1} but it had no effect.",name , target.name);
                 }
                 Discard(0);
             }
--- Program.cs
+++ Program.cs
@@ -12,149 +12,235 @@ namespace CardGame
             while (Game)
             {
                 Deck GameDeck = new Deck();
-                System.Console.WriteLine("WELCOME TO WAR 2018");
-                System.Console.WriteLine("###########");
-                // Deck GameDeck = new Deck();
-                // System.Console.WriteLine("WELCOME TO WAR 2018");
-                // System.Console.WriteLine("###########");
-                // System.Console.WriteLine("Select 1-2 Players");
-                // string Boop = Console.ReadLine();
-                // int Players = Int32.Parse(Boop);
-                // int cardsNeeded = 0;
-                System.Console.WriteLine("Enter Your Name: ");
-                Boop = Console.ReadLine();
-                    System.Console.WriteLine("###########");
-                    System.Console.WriteLine("###########");
-                    System.Console.WriteLine("###########");
-                System.Console.WriteLine("To exit input 0");
+                System.Console.WriteLine("       ************************************       ");
+                System.Console.WriteLine("   ********************************************   ");
+                System.Console.WriteLine(" **********************************************  ");
+                System.Console.WriteLine("*************************************************");
+                System.Console.WriteLine("***             *******************           ***");
+                System.Console.WriteLine("***             WELCOME TO WAR 2018           ***");
+                System.Console.WriteLine("***             *******************           ***");
+                System.Console.WriteLine("*************************************************");
+                System.Console.WriteLine("*********************          ******************");
+                System.Console.WriteLine("*********************          ******************");
+                System.Console.WriteLine(" ************************************************");
+                System.Console.WriteLine("  **********  ****  ****  ****  ****  **********  ");
+                System.Console.WriteLine("   *********  ****  ****  ****  ****  *********  ");
+                System.Console.WriteLine("    ********  ****  ****  ****  ****  ********  ");
+                System.Console.WriteLine("    ********  ****  ****  ****  ****  *******  ");
+                System.Console.WriteLine("_______________________________________________");
+                System.Console.WriteLine("***   Select 1-2 Players");
+                int cardsNeeded = 0;
+                int Players = 0;
 
-                Player player1 = new Player(Boop);
-                int cardsNeeded = 3;
-                Boss EO = new Boss();
+                Player player1 = new Player ("Temp");
+                Player player2 = player1;
+                Player EO = player1;
 
-                // if (Players == 1)
-                // {
-                //     System.Console.WriteLine("###########");
-                //     System.Console.WriteLine("Enter Your Name: ");
-                //     Boop = Console.ReadLine();
-                //     Player player1 = new Player(Boop);
-                //     cardsNeeded = 3;
-                //     // create boss with 100hp
-                // }
-                // else if (Players == 2)
-                // {
-                //     System.Console.WriteLine("###########");
-                //     System.Console.WriteLine("Enter Player 1's Name: ");
-                //     Boop = Console.ReadLine();
-                //     Player player1 = new Player(Boop);
-                //     System.Console.WriteLine("###########");
-                //     System.Console.WriteLine("Enter Player 2's Name: ");
-                //     Boop = Console.ReadLine();
-                //     Player player2 = new Player(Boop);
-                //     // create boss with 200hp
-                // }
-                // else
-                // {
-                //     System.Console.WriteLine("You sure ###### up now, huh?");
-                //     Game = false;
-                // }
-
-                while (player1.health > 0 && EO.health > 0)//and boss health
+                while (Players == 0)
                 {
-                    System.Console.WriteLine("###########");
-                    System.Console.WriteLine("Your health is: {0}", player1.health);
-                    System.Console.WriteLine("Boss's health is: {0}", EO.health);
-                    System.Console.WriteLine("###########");
+                    Boop = Console.ReadLine();
+                    Players = Int32.Parse(Boop);
+                    if (Players == 1)
+                    {
+                        System.Console.WriteLine("*************************************************");
+                        System.Console.WriteLine("Enter Your Name: ");
+                        Boop = Console.ReadLine();
+                        player1 = new Player(Boop);
+                        cardsNeeded = 4;
+                        EO = new Player("Evil Overlord", 100);
+                    }
+                    else if (Players == 2)
+                    {
+                        System.Console.WriteLine("*************************************************");
+                        System.Console.WriteLine("***   Enter Player 1's Name: ");
+                        Boop = Console.ReadLine();
+                        player1 = new Player(Boop);
+                        System.Console.WriteLine("*************************************************");
+                        System.Console.WriteLine("***   Enter Player 2's Name: ");
+                        Boop = Console.ReadLine();
+                        player2 = new Player(Boop);
+                        EO = new Player("Evil Overlord", 200);
+                        cardsNeeded = 6;
+                    }
+                    else
+                    {
+                        Players = 0;
+                    }
+                    System.Console.WriteLine("Please type 1 or 2.");
+                }
 
-                    EO.DrawCard(GameDeck);
-                    // System.Console.WriteLine("The {0} drew a card.", EO.name);
-                    System.Console.WriteLine("Drawing Cards");
+                int PlayersAlive = Players;
+
+                //game loop
+                while (PlayersAlive > 0 && EO.health > 0)//and boss health
+                {
+                    System.Console.WriteLine("*************************************************");
+                    System.Console.WriteLine("***   {0} health is: {1}", player1.name, player1.health);
+                    if (Players >1)
+                    {
+                    System.Console.WriteLine("***   {0} health is: {1}", player2.name, player2.health);
+                    }
+                    System.Console.WriteLine("***   Boss's health is: {0}", EO.health);
+                    System.Console.WriteLine("*************************************************");
+
+                    EO.Draw(GameDeck);
+                    EO.Draw(GameDeck);
+
+                    if ((Players>1 && player1.health >= 0) || Players == 1)
+                    {
                     player1.Draw(GameDeck);
                     player1.Draw(GameDeck);
-                    // System.Console.WriteLine("You drew two cards.");
-                    System.Console.WriteLine("###########");
+                    System.Console.WriteLine("***      {0}'s Turn", player1.name);
+                    System.Console.WriteLine("*************************************************");
                     player1.DisplayHand();
-                    System.Console.WriteLine("###########");
-                    System.Console.WriteLine("Would you like to play the first card or the second card?");
-                    System.Console.WriteLine("Type 'A' for the first card and 'B' for the second card.");
+                    System.Console.WriteLine("*************************************************");
+                    System.Console.WriteLine("***   Would you like to play the first card or the second card?");
+                    System.Console.WriteLine("***   Type 'A' for the first card and 'B' for the second card.");
 
                     bool errorCheck = true;
                     while (errorCheck == true)
                     {
                     Boop = Console.ReadLine();
-                    System.Console.WriteLine("###########");
-                    System.Console.WriteLine("###########");
-                    System.Console.WriteLine("###########");
+                    System.Console.WriteLine("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+                    System.Console.WriteLine("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+                    System.Console.WriteLine("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
 
                         if (Boop == "A" || Boop == "a")
                         {
                             player1.Attack(player1.Discard(0), EO);
-
-                            // Card cardplayed = player1.Discard(0);
-                            // cardplayed.Attack(EO);
-                            // player1.Discard(0);
                             errorCheck = false;
                         }
                         else if(Boop == "B" || Boop == "b")
                         {
                             player1.Attack(player1.Discard(1), EO);
-                            
-                            // Card cardplayed = player1.Discard(1);
-                            // cardplayed.Attack(EO);
                             errorCheck = false;
                         }
                         else
                         {
                             System.Console.WriteLine("Try writing 'a' or 'b' please.");
                         }
+                        if (EO.health <= 0 && Players>1)
+                        {
+                            System.Console.WriteLine("You would win now but we need to work on that so just keep going.");
+                        }
                     }
 
-                    System.Console.WriteLine("###########");
-                    // Card cardplayed1 = EO.Discard();
-                    // cardplayed1.Attack(player1);
-                    EO.Attack(EO.Discard(), player1);
-                    System.Console.WriteLine("###########");
+                    System.Console.WriteLine("*************************************************");
+                    }
+
+//player 2
+                    if (Players>1 && player2.health >= 0)
+                    {
+                    System.Console.WriteLine("***   {0} health is: {1}", player1.name, player1.health);
+                    if (Players >1)
+                    {
+                    System.Console.WriteLine("***   {0} health is: {1}", player2.name, player2.health);
+                    }
+                    System.Console.WriteLine("***   Boss's health is: {0}", EO.health);
+                    System.Console.WriteLine("*************************************************");
+                    player2.Draw(GameDeck);
+                    player2.Draw(GameDeck);
+                    // System.Console.WriteLine("You drew two cards.");
+                    System.Console.WriteLine("***      {0}'s Turn", player2.name);
+                    System.Console.WriteLine("*************************************************");
+                    player2.DisplayHand();
+                    System.Console.WriteLine("*************************************************");
+                    System.Console.WriteLine("***   Would you like to play the first card or the second card?");
+                    System.Console.WriteLine("***   Type 'A' for the first card and 'B' for the second card.");
+
+                    bool errorCheck3 = true;
+                    while (errorCheck3 == true)
+                    {
+                    Boop = Console.ReadLine();
+
+                        if (Boop == "A" || Boop == "a")
+                        {
+                            player2.Attack(player2.Discard(0), EO);
+                            errorCheck3 = false;
+                        }
+                        else if(Boop == "B" || Boop == "b")
+                        {
+                            player2.Attack(player2.Discard(1), EO);
+                            errorCheck3 = false;
+                        }
+                        else
+                        {
+                            System.Console.WriteLine("Try writing 'a' or 'b' please.");
+                        }
+                    }
+                    }
+
+                    System.Console.WriteLine("*************************************************");
+                    
+                    // pick a player
+                    Player chosenPlayer = player1;
+                    if (Players > 1)
+                    {
+                    Random rand = new Random();
+                    int x = rand.Next(1,3);
+                    if (x == 2)
+                    {
+                        chosenPlayer = player2;
+                    }
+                    }
+
+                    EO.Attack(EO.Discard(0), chosenPlayer);
+                    System.Console.WriteLine("*************************************************");
                     
                     if (GameDeck.cards.Count < cardsNeeded)
                     {
                         System.Console.WriteLine("Re-Shuffling Deck");
                         GameDeck.Reset();
                     }
+
+                    if (player1.health <=0)
+                    {
+                        System.Console.WriteLine("{0} has died", player1.name);
+                        PlayersAlive--;
+                    }
+                    else if (player2.health <=0)
+                    {
+                        System.Console.WriteLine("{0} has died", player2.name);
+                        PlayersAlive--;
+                    }
                 }
 
                 // if someone won say who won
                 if (EO.health <= 0)
                 {
-                    System.Console.WriteLine("****************************");
-                    System.Console.WriteLine("*    Fuck yeah you won.    *");
-                    System.Console.WriteLine("****************************");
+                    System.Console.WriteLine("***********************************");
+                    System.Console.WriteLine("*    The Evil Overlord Is Dead    *");
+                    System.Console.WriteLine("*     You Are The New Overlord    *");
+                    System.Console.WriteLine("*         Destroy It All          *");
+                    System.Console.WriteLine("***********************************");
+                    // player who won?
                 }
-                else if (player1.health <= 0)
+                else if (PlayersAlive <= 0)
                 {
-                    System.Console.WriteLine("You died. Very dead.");
+                    System.Console.WriteLine("You died. Very dead. Evil Wins Again.");
                 }
                 else
                 {
                     System.Console.WriteLine("How the hell?");
                 }
 
-                System.Console.WriteLine("Type a to continue and b to quit.");
+                System.Console.WriteLine("Type 'C' to play again and 'Q' to quit.");
                 bool errorCheck1 = true;
                 while (errorCheck1)
                 {
                     Boop = Console.ReadLine();
-                    if (Boop == "b" || Boop == "B")
+                    if (Boop == "q" || Boop == "Q")
                     {
                         Game = false;
                         errorCheck1 = false;
                     }
-                    else if (Boop == "a" || Boop == "A")
+                    else if (Boop == "c" || Boop == "C")
                     {
                         errorCheck1 = false;
                     }
                     else
                     {
-                    System.Console.WriteLine("Type a to continue and b to quit.");
+                    System.Console.WriteLine("Type 'C' to play again and 'Q' to quit.");
                     }
                 }
             }
